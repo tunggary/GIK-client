e556b6220811a5ad2bc51f638d8275f1ea9966ac
tunggary <gsy1656@gmail.com>
2021-12-28 17:41:43 +0900

    Add prettier, eslintrc

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,6 @@
-import React from "react";
-import styled from "styled-components";
-import Banner from "../components/main/Banner";
+import React from 'react';
+import styled from 'styled-components';
+import Banner from '../components/main/Banner';
 
 export default function main() {
   return (
