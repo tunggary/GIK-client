29617708fa6fe6b9ee02bd5c117239beca6cabc4
tunggary <gsy1656@gmail.com>
2021-12-28 17:44:40 +0900

    Add prettier, eslintrc

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,5 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Main from "./pages/Main";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from './pages/Main';
 
 export default function Router() {
   return (
